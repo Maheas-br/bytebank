888a27f98d30bd9eb99673d241772c1fe304c97a
Matheus Henrique de Almeida Souza <72990173+Maheas-br@users.noreply.github.com>
2021-12-08 15:34:05 -0300

    Exportando dados do componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+
 
 @Component({
     selector:'app-nova-transferencia',
@@ -8,13 +9,23 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent{
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+
   valor : number = 12;
   destino : number = 222;
 
   transferir(){
     console.log('Solicitada nova transferência');
-    console.log('Valor:',this.valor);
-    console.log('Destino:',this.destino);
 
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0 ;
+    this.destino = 0 ;
   }
 }
